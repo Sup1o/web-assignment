bbe4b94e4010b2c62d3a0d8b1f44b8a0941fc189
HCrystalH <hvvh190102@gmail.com>
2023-05-11 16:30:48 +0700

    update home_page css

--- pages/home1.php
+++ pages/home1.php
@@ -27,51 +27,17 @@
 		}
 		input[type=submit]:hover {
 			background-color: #45a049;
-		}
-		.company {
-            border: 1px solid black;
-            padding: 10px;
-            margin-bottom: 10px;
-            display: grid;
-            grid-template-columns: 335px 335px;
-            grid-template-rows: repeat(auto);
-            grid-gap: 10px;
         }
 
-        .company h2 {
-            margin-top: 0;
-            grid-column: 1 / span 2;
-        }
-
-        .company p {
-            margin: 0;
-            border: 1px solid black;
-            padding: 10px;
-            text-align: center;
-        }
-		.company {
-            border: 1px solid black;
-            padding: 10px;
-            margin-bottom: 10px;
-            display: grid;
-            grid-template-columns: 335px 335px;
-            grid-template-rows: repeat(auto);
-            grid-gap: 10px;
-        }
-
-        .company h2 {
-            margin-top: 0;
-            grid-column: 1 / span 2;
-        }
-
-        .company p {
-            margin: 0;
-            border: 1px solid black;
-            padding: 10px;
-            text-align: center;
-        }
+       
 	</style>
-	<link rel="stylesheet" href="./pages/styles.css">
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./pages/styles.css">
+    <link rel="stylesheet" href="./pages/home_css.css"> 
+
+    <link rel="stylesheet" href ="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 </head>
 <header>
     <div class = "logo" >CV MANAGEMENT</div>
@@ -93,14 +59,53 @@
         </li>
     </ul>
 </header>
-<body>
-<div class="home_container">
-<h1>Welcome to CV Management site</h1>
-        <p>hello jobseekers!</p>
-        <p>Welcome to our job management platform. You can use this site to find job listings, create your CV, and apply for jobs. We wish you the best of luck in your job search!</p>
-</div>
-</body>
 
+<body>
+<div style="min-height:220vh; background-color: aliceblue;">
+<div class="hello">
+    <p>WELCOME TO CV MANAGEMENT SITE<p>
+</div>
+
+<div class ="intro"> 
+    <p> Our website offers a CV management system to help job seekers manage
+    and organize their resumes more efficiently.</p>
+</div>
+
+<div class="service-container" >
+    
+    <div class ="service-content">
+        <a href="./index.php?page=home" title="GO TO HOME PAGE"> 
+            <div class ="box"> 
+                <i class='bx bx-home-alt'></i>
+            </div>
+        </a>
+        <a href="./index.php?page=candidate_search" title = "CVMangement candidate search">
+            <div class =" box"> 
+                <i class='bx bx-search-alt-2'></i>
+            </div>
+        </a> 
+       
+    </div>
+</div>
+
+</div>
+
+<footer>
+    <div class ="mssv">
+        <div class ="name" style="margin-right: auto;">
+            <p> Huỳnh Tuấn Kiệt-2052561</p>
+            <p> Hoàng Vương Vũ Hoàng-2052477 </p>
+            <p> Đặng Quốc Thịnh-1852761 </p>
+            <p> Đỗ Hoàng Hiếu-1952678</p>
+        </div>
+        <div class ="lop">
+            <p> Semester: 222 </p>
+            <p> CO3050 - Class: CC01 </p>
+            <p> Instructor: Nguyễn Đức Thái </p>
+        </div>
+    </div>
+</footer>
+</body>
 
 <script>
     function myFunction() {
